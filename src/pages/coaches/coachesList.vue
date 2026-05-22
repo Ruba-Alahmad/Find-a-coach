9f8c16e95cade4382423e74c6bce1c80b37a1f54
Ruba-Alahmad <rubaalahmad393@gmail.com>
2026-05-22 20:31:30 +0300

    Add coach data

--- src/pages/coaches/coachesList.vue
+++ src/pages/coaches/coachesList.vue
@@ -5,8 +5,25 @@
             <button>Refresh</button>
             <router-link to="/register">Register as Coach</router-link>
         </div>
-        <ul>
-        <li>List of coaches</li>
+        <ul v-if="hasCoaches">
+        <li v-for="coach in filteredCoaches" :key="coach.id">
+            {{coach.firstName}}
+        </li>
         </ul>
+        <h3 v-else>No Coaches found</h3>
     </section>
 </template>
+
+
+<script>
+export default {
+    computed:{
+        filteredCoaches(){
+            return this.$store.getters['coaches/coaches'];
+        },
+        hasCoaches(){
+            return this.$store.getters['coaches/hasCoaches'];
+        }
+    }
+}
+</script>
