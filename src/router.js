ba8882fa6e1da8874fa969d8a6f59229fd91536f
Ruba-Alahmad <rubaalahmad393@gmail.com>
2026-05-22 18:16:14 +0300

    Initial main branch

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import { createRouter , createWebHistory} from "vue-router";
+
+
+import coachDetails from './pages/coaches/coachDetails.vue';
+import coachesList from './pages/coaches/coachesList.vue';
+import coachRegistration from './pages/coaches/coachRegistration.vue';
+import contactCoach from './pages/requests/contactCoach.vue';
+import requestsReceived from './pages/requests/requestsReceived.vue';
+import notFound from './pages/notFound.vue';
+
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes:[
+        {path: '/', redirect: '/coaches'},
+        {path: '/coaches', component: coachesList},
+        {path: '/coaches/:id', component: coachDetails, children:[
+                {path: '/contact', component: contactCoach},
+            ]},
+        {path: '/register', component: coachRegistration},
+        {path: '/requests', component: requestsReceived},
+        {path: '/notFound(.*)', component: notFound},
+    ]
+});
+export default router;
