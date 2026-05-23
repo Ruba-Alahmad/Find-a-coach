d12a47fae9c623a0ad0e8c46931cd631e77f9363
Ruba-Alahmad <rubaalahmad393@gmail.com>
2026-05-23 15:28:59 +0300

    Registering as a Coach

--- src/pages/coaches/coachRegistration.vue
+++ src/pages/coaches/coachRegistration.vue
@@ -1,3 +1,27 @@
 <template>
-    Register
+    <section>
+        <base-card>
+         <h2>Register as a Coach now!</h2>
+         <coach-form @save-data="saveData"></coach-form>
+        </base-card>
+    </section>
 </template>
+
+<script>
+import coachForm from '../../components/coaches/coachForm.vue'
+
+export default {
+    components:{
+        coachForm,
+    },
+    methods:{
+        saveData(data){
+            this.$store.dispatch('coaches/registerCoach', data);
+            this.$router.replace('/coaches')
+        }
+    },
+    computed:{
+    
+    }
+}
+</script>
