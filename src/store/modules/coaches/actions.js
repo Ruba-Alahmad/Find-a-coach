b886c3b3e8597e5568f2a85cb85e1e829743d64c
Ruba-Alahmad <rubaalahmad393@gmail.com>
2026-06-06 14:01:15 +0300

    Attaching the token to outgoing requests

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -8,8 +8,13 @@ export default {
             hourlyRate: data.rate,
             areas: data.areas
         };
+        
+        const token = context.rootGetters.token;
 
-        const response = await fetch(`https://find-a-coach-fbc81-default-rtdb.firebaseio.com/coaches/${userId}.json`,{
+        const response = await fetch(
+            `https://find-a-coach-fbc81-default-rtdb.firebaseio.com/coaches/${userId}.json?auth=`
+             + token
+            ,{
             method: 'PUT',
             body:JSON.stringify(coachData)
         });
